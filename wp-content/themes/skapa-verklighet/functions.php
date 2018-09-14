bf68396f1c52486167e94052934fe74a3c4398ae
Richard Bång <richard.bang@live.se>
2018-09-14 10:01:07 +0200

    css version

--- wp-content/themes/skapa-verklighet/functions.php
+++ wp-content/themes/skapa-verklighet/functions.php
@@ -147,7 +147,7 @@ function cone_enqueue_scripts() {
     wp_enqueue_script( 'swiper-scripts', get_template_directory_uri() . '/assets/js/lib/swiper.min.js', array('jquery'), 1.0, true );
 
     // vendor.css created with gulp
-    wp_enqueue_style( 'main-min-style', get_template_directory_uri() . '/assets/css/main.min.css' );
+    wp_enqueue_style( 'main-min-style', get_template_directory_uri() . '/assets/css/main.min.css', array(), 1.1 );
 
     // vendor.js created with gulp
     wp_enqueue_script( 'main-min-scripts', get_template_directory_uri() . '/assets/js/src/main.min.js', array('jquery'), 1.0, true );
