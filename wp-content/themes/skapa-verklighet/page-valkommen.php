d5e6ed8bd0e48ade0ed18667e77e407349c890ea
Richard Bång <richard.bang@live.se>
2019-01-10 11:26:54 +0100

    köpa kurser från admin

--- wp-content/themes/skapa-verklighet/page-valkommen.php
+++ wp-content/themes/skapa-verklighet/page-valkommen.php
@@ -2,9 +2,9 @@
 
 
 <?php if( rcp_is_active() ) : ?>
-
     <section class="cd-home-courses cd-welcome-courses cd-home-section">
         <div class="large-container">
+            <a class="cd-back-to-home-link" href="<?php echo esc_url(home_url()); ?>"><i class="material-icons">arrow_back</i>Tillbaks till startsidan</a>
             <h3>Hej <?php echo do_shortcode('[user_name]') ; ?>, här är dina tillgängliga webbkurser</h3>
             <div class="cd-home-courses-grid">
                 <?php $loop = new WP_Query( array(
@@ -19,20 +19,28 @@
                 )); ?>
                 <?php if ( $loop->have_posts() ) : ?>
                     <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-                <?php if( rcp_user_can_access( get_current_user_id(), get_the_ID() ) ) : ?>
-
-                    <div class="cd-home-courses-card <?php if(get_field('course-disabled')) : ?> cd-home-courses-card-disabled <?php endif ; ?>">
-                            <div class="cd-home-courses-card-top background-img" style="background-image: url('<?php the_post_thumbnail_url() ; ?>')">
-                                <a href="<?php the_permalink() ; ?>"></a>
-                                <div class="background-overlay" style="background: <?php the_field('page-course-gradient') ; ?>;"></div>
-                                <span><?php the_field('page-course-part') ; ?></span>
-                                <h4><?php the_title() ; ?></h4>
-                                <div class="cd-comming-soon-tile">
-                                    <p>Kommer snart</p>
+                        <?php if( rcp_user_can_access( get_current_user_id(), get_the_ID() ) ) : ?>
+                            <div class="cd-home-courses-card">
+                                <div class="cd-home-courses-card-top background-img" style="background-image: url('<?php the_post_thumbnail_url() ; ?>')">
+                                    <a href="<?php the_permalink() ; ?>"></a>
+                                    <div class="background-overlay" style="background: <?php the_field('page-course-gradient') ; ?>;"></div>
+                                    <span><?php the_field('page-course-part') ; ?></span>
+                                    <h4><?php the_title() ; ?></h4>
                                 </div>
                             </div>
-                        </div>
-                    <?php endif ;?>
+                        <?php else : ?>
+                            <div class="cd-home-courses-card cd-home-courses-card-disabled">
+                                <div class="cd-home-courses-card-top background-img" style="background-image: url('<?php the_post_thumbnail_url() ; ?>')">
+                                    <a href="<?php the_permalink() ; ?>"></a>
+                                    <div class="background-overlay" style="background: <?php the_field('page-course-gradient') ; ?>;"></div>
+                                    <span><?php the_field('page-course-part') ; ?></span>
+                                    <h4><?php the_title() ; ?></h4>
+                                    <div class="cd-comming-soon-tile cd-show-tile">
+                                        <p>Köp Kursen</p>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endif ;?>
                     <?php endwhile; ?>
                 <?php endif; ?>
                 <?php wp_reset_query(); ?>
